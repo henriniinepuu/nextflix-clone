22843b83ab5be379a95b84bc12b108d8e1fb0dad
Henri Niinepuu <henriniinepuu@gmail.com>
2025-01-26 22:16:39 +0200

    Added Google and Github login. But there seems to be an error with Sessions or with creating new user from SSO button

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -6,6 +6,9 @@ import { useCallback, useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
 const AuthPage = () => {
     const router = useRouter();
     const [email, setEmail] = useState("");
@@ -89,6 +92,19 @@ const AuthPage = () => {
                         <button onClick={variant === "register" ? register : login} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition font-semibold">
                             {variant === "login" ? "Login" : "Register"}
                         </button>
+                        <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                            <div
+                                onClick={() => signIn("google", { callbackUrl: "/" })}
+                                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                <FcGoogle size={30} />
+                            </div>
+                            <div 
+                                onClick={() => signIn("github", { callbackUrl: "/" })}
+                                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+                            >
+                                <FaGithub size={30} />
+                            </div>
+                        </div>
                         <p className="text-neutral-500 mt-8 text-center">
                             {variant === "login" ? "First time using Netflix?" : "Already have an account?"}
                             <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-
 import "./globals.css";
+import { Providers } from "./providers";
 
 export default function RootLayout({
   children,
@@ -9,7 +9,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
